981963b45b545553bc3ed7794e17e91be6d333e2
Prashanth Rotti H <32154958+PrashanthRotti@users.noreply.github.com>
2022-05-30 11:34:07 +0530

    Create actionType.js

---
+++ src/redux/actionType.js
@@ -0,0 +1,4 @@
+export const GET_MOVIES = "GET_MOVIES";
+export const GET_SINGLE_MOVIE = "GET_SINGLE_MOVIE";
+export const GET_SEARCH_MOVIE = "GET_SEARCH_MOVIE";
+export const RESET_LOADER = "RESET_LOADER";
