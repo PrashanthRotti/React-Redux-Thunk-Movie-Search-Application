315a9c0194bb1288a3f8aa5b48bffa9cc632af15
Prashanth Rotti H <32154958+PrashanthRotti@users.noreply.github.com>
2022-05-30 11:35:35 +0530

    Create Loading.js

---
+++ src/pages/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../movie.css';
+
+const Loading = (props) => {
+    return (
+        <div className="loader loader-bg-black"></div>
+    )
+}
+
+export default Loading;
